f257d6086b08e551daffec8240043ec97ed50810
heyuanxiang <yuanxiang.he@uwa4d.com>
2023-03-06 18:51:55 +0800

    feat: adapter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import type { AxiosRequestConfig } from 'axios'
-import axios from 'axios'
+import axios, { type AxiosRequestConfig } from 'axios'
+import hash from './utils/hash'
+import cache from './utils/cache'
 
 const adapter = (config: AxiosRequestConfig) => {
-  console.log('>>> using custom adapter')
-  return axios.defaults.adapter(config)
+  const key = hash(config)
+  const target = cache.get(key)
+  if (target) return target
+  const request = axios.defaults.adapter(config)
+  cache.set(key, request)
+  return request
 }
 
 export default adapter
